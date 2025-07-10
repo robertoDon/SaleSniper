a3598e86f9eac9dac6f8e4f80cc923f0999c9fcc
roberto <roberto@don.adm.br>
2025-07-10 15:45:39 -0300

    Ajustes para reconhecer como pacotes

--- scr/streamlit_app.py
+++ scr/streamlit_app.py
@@ -8,7 +8,7 @@ from components.churn import exibir_churn
 # Importação comentada para MVP inicial
 # from components.tamsamsom import exibir_tamsamsom
 import os
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from PIL import Image
 
 # Carregando a logo
