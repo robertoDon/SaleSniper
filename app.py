bffb44a9c1646b7caeb06dfa82613508cf1a00e3
robertoDon <roberto@don.adm.br>
2026-03-17 18:47:17 -0300

    feat: Configure application to run with Gunicorn and set environment-specific port

--- app.py
+++ app.py
@@ -166,4 +166,5 @@ def admin():
     return render_template('admin.html', usuarios=usuarios, user=current_user.id)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
